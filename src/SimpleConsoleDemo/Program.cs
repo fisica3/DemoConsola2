484927e512a5864176e7aef0617056df8f0469b8
Ernesto Cárdenas <ernesto.cardenas@clarabridge.com>
2016-08-09 12:52:52 -0500

    Primer Commit

--- src/SimpleConsoleDemo/Program.cs
+++ src/SimpleConsoleDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,6 +10,9 @@ namespace SimpleConsoleDemo
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Hola comunidad Docker");
+            string entrada = Console.ReadLine();
+            Console.WriteLine("Gracias {0}", entrada);
         }
     }
 }
